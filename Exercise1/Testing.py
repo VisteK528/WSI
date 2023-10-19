ab34d9fa89ca246984471751691bdc12c2c16764
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-10-19 17:47:52 +0200

    Cleaned code, reimplemented grad_step method so that it generally represents one step of simple gradient

--- Exercise1/Testing.py
+++ Exercise1/Testing.py
@@ -2,6 +2,7 @@ from typing import Callable, Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class SimpleGradientDescent:
     X = np.arange(-2, 2, 0.05)
     Y = np.arange(-3, 2, 0.05)
@@ -10,7 +11,7 @@ class SimpleGradientDescent:
     def __init__(self,
                  func: Callable[[float, float], float],
                  grad_func: Callable[[float, float], Tuple[float, float]],
-                 alpha:float=0.1):
+                 alpha: float = 0.1):
         self.alpha = alpha
         self.func = func
         self.grad_func = grad_func
@@ -24,32 +25,33 @@ class SimpleGradientDescent:
         plt.figure()
         plt.contour(self.X, self.Y, self.Z, 50)
         if self.trace is not None:
-            if len(self.trace)>0:
-                plt.scatter(self.trace[:,0], self.trace[:,1], s=10)
+            if len(self.trace) > 0:
+                plt.plot(self.trace[:, 0], self.trace[:, 1], color="red")
 
-    def calculate_func_vale(self, x1:float, x2:float) -> float:
-        pass
+    def calculate_func_vale(self, x1: float, x2: float) -> float:
+        return self.func(x1, x2)
 
-    def calculate_func_grad(self, x1:float, x2:float) -> Tuple[float, float]:
+    def calculate_func_grad(self, x1: float, x2: float) -> Tuple[float, float]:
         return self.grad_func(x1, x2)
 
-    def gradient_descent_step(self, x1:float, x2:float) -> Tuple[float, float]:
-        if self.trace is not None:
-            return np.array(x1, x2) - self.alpha * np.array(self.calculate_func_grad(x1, x2))
-        else:
-            return (x1, x2)
+    def gradient_descent_step(self, x1: float, x2: float) -> Tuple[
+        float, float]:
+        return np.array(x1, x2) - self.alpha * np.array(
+            self.calculate_func_grad(x1, x2))
 
-    def minimize(self, x1_init:float, x2_init:float, steps:int, verbose:int=0, plot:bool=False)->float:
-        for i in range(steps):
-            if self.trace is None:
-                result = self.gradient_descent_step(x1_init, x2_init)
-                self.trace = np.empty((steps, 2))
-                self.trace[i, :] = result
-            else:
-                result = self.gradient_descent_step(self.trace[i-1, 0], self.trace[i-1, 1])
-                self.trace[i, :] = result
+    def minimize(self, x1_init: float, x2_init: float, steps: int,
+                 verbose: int = 0, plot: bool = False) -> float:
+        self.trace = np.array([[x1_init, x2_init], ])
 
-        self.trace = self.trace.reshape((2, steps))
+        for i in range(1, steps):
+            result = self.gradient_descent_step(self.trace[i - 1, 0],
+                                                self.trace[i - 1, 1])
+            self.trace = np.append(self.trace, np.array([result]), axis=0)
+
+        if plot:
+            self.plot_func()
+
+        return self.calculate_func_vale(self.trace[-1, 0], self.trace[-1, 1])
 
 
 class Function1:
