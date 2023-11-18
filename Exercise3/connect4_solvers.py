297ca2f284c9e42256849c31d095c92ceb67a7c7
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-11-18 23:38:57 +0100

    Final version of Exercise3

--- Exercise3/connect4_solvers.py
+++ Exercise3/connect4_solvers.py
@@ -5,6 +5,7 @@ from two_player_games.games.connect_four import ConnectFour, ConnectFourMove, \
 from copy import copy
 from random import choice
 import numpy as np
+import time
 
 
 def simulate_move(board, column):
@@ -14,39 +15,41 @@ def simulate_move(board, column):
 
 
 class Solver:
-    def __init__(self, game: ConnectFour, row_count: int, column_count: int):
+    def __init__(self, game: ConnectFour, row_count: int, column_count: int, max_player: Player, min_player: Player):
         self.game = game
         self._row_count = row_count
         self._column_count = column_count
 
+        self._max_player = max_player
+        self._min_player = min_player
+
     def is_valid_move(self, col_index: int) -> bool:
-        """
-        Check if move to column is available
-        """
+        # Check if move to column is available
         if self.game.state.fields[col_index][-1] is None:
             return True
         else:
             return False
 
     def get_valid_moves(self):
-        """
-        Return list of all currently available moves
-        """
+        # Return list of all currently available moves
         return [valid_column for valid_column in range(self._column_count) if
                 self.is_valid_move(valid_column)]
 
+    def get_best_move(self, *args):
+        pass
+
 
 class RandomSolver(Solver):
-    def __init__(self, game: ConnectFour, row_count: int, column_count: int):
-        super().__init__(game, row_count, column_count)
+    def __init__(self, game: ConnectFour, row_count: int, column_count: int, max_player: Player, min_player: Player):
+        super().__init__(game, row_count, column_count, max_player, min_player)
 
-    def get_random_move(self):
+    def get_best_move(self, *args):
         return choice(self.get_valid_moves())
 
 
 class HeuristicSolver(Solver):
-    def __init__(self, game: ConnectFour, row_count: int, column_count: int):
-        super().__init__(game, row_count, column_count)
+    def __init__(self, game: ConnectFour, row_count: int, column_count: int, max_player: Player, min_player: Player):
+        super().__init__(game, row_count, column_count, max_player, min_player)
 
         self._prizes = {
             "plr_two_in_seg": 5,
@@ -92,65 +95,67 @@ class HeuristicSolver(Solver):
     def _evaluate_segment(self, segment: List, player: Player):
         prize = 0
 
-        opponent = self.game.second_player
-        if player == self.game.second_player:
-            opponent = self.game.first_player
-
         # Evaluate max player and min player possibility
         # for winning combination in the future
-        if segment.count(player) == 1 and segment.count(None) == 3:
+        if segment.count(self._max_player) == 1 and segment.count(None) == 3:
             prize += 1
-        elif segment.count(opponent) == 1 and segment.count(None) == 3:
+        elif segment.count(self._min_player) == 1 and segment.count(None) == 3:
             prize -= 1
 
         # Evaluate already existing combinations
-        if segment.count(player) == 4:
+        if segment.count(self._max_player) == 4:
             prize += self._prizes["plr_winning_cond"]
-        elif segment.count(player) == 3 and segment.count(None) == 1:
+        elif segment.count(self._max_player) == 3 and segment.count(None) == 1:
             prize += self._prizes["plr_three_in_seg"]
-        elif segment.count(player) == 2 and segment.count(None) == 2:
+        elif segment.count(self._max_player) == 2 and segment.count(None) == 2:
             prize += self._prizes["plr_two_in_seg"]
-        elif segment.count(opponent) == 3 and segment.count(None) == 1:
+        elif segment.count(self._min_player) == 3 and segment.count(None) == 1:
             prize -= self._prizes["opp_winning_cond"]
 
         return prize
 
-    def get_heuristic_move(self):
+    def get_best_move(self, *args):
         scores = []
         for move in self.get_valid_moves():
             board_copy = simulate_move(self.game.state, move)
-            scores.append((move, self.evaluate_position(board_copy, self.game.state.get_current_player())))
+            scores.append((move, self.evaluate_position(board_copy, self._max_player)))
 
         moves = [score[0] for score in scores if score[1] == max([score[1] for score in scores])]
         return choice(moves)
 
 
 class MinMaxSolver(HeuristicSolver):
-    def __init__(self, game: ConnectFour, row_count: int, column_count: int):
-        super().__init__(game, row_count, column_count)
+    def __init__(self, game: ConnectFour, row_count: int, column_count: int,
+                 max_player: Player, min_player: Player):
+        super().__init__(game, row_count, column_count, max_player, min_player)
 
-    def get_best_move_alpha_beta(self, depth: int) -> int:
-        col, score = self.minimax_alpha_beta(self.game.state, depth, -np.inf, np.inf,
-                                        True)
-        return col
+    def get_best_move(self, depth, alpha_beta=True, *args):
+        if alpha_beta:
+            col, score = self.minimax_alpha_beta(self.game.state, depth,
+                                                 -np.inf, np.inf,
+                                                 True)
+            return col
+        else:
+            col, score = self.minimax(self.game.state, depth, True)
+            return col
 
     def minimax_alpha_beta(self, board, depth, alpha: float, beta: float,
                 is_maximizing_player: bool) -> Tuple[int, float]:
-        """Returns column index and score"""
+        # Returns column index and score
         valid_moves = self.get_valid_moves()
         is_terminal = board.is_finished()
 
         if is_terminal or depth == 0:
             if is_terminal:
-                if board.get_winner() is not None:
-                    if is_maximizing_player:
-                        return (None, 1e10)
-                    else:
-                        return (None, -1e10)
+                if board.get_winner() == self._max_player:
+                    return (None, 1e10)
+                elif board.get_winner() == self._min_player:
+                    return (None, -1e10)
                 else:
                     return (None, 0)
             else:
-                return (None, self.evaluate_position(board, board.get_current_player()))
+                return (
+                    None, self.evaluate_position(board, self._max_player))
 
         if is_maximizing_player:
             value = -np.inf
@@ -186,4 +191,36 @@ class MinMaxSolver(HeuristicSolver):
 
             return chosen_column, value
 
+    def minimax(self, board, depth, is_maximizing_player: bool) -> Tuple[int, float]:
+        """Returns column index and score"""
+        valid_moves = self.get_valid_moves()
+        is_terminal = board.is_finished()
 
+        if is_terminal or depth == 0:
+            if is_terminal:
+                if board.get_winner() == self._max_player:
+                    return (None, 1e10)
+                elif board.get_winner() == self._min_player:
+                    return (None, -1e10)
+                else:
+                    return (None, 0)
+            else:
+                return (
+                    None, self.evaluate_position(board, self._max_player))
+
+        evaluations = []
+        for valid_move in valid_moves:
+            evaluation = self.minimax(simulate_move(board, valid_move), depth - 1, not is_maximizing_player)[1]
+            evaluations.append((valid_move, evaluation))
+
+        if is_maximizing_player:
+            moves = [score for score in evaluations if
+                     score[1] == max([score[1] for score in evaluations])]
+
+            return choice(moves)
+
+        else:
+            moves = [score for score in evaluations if
+                     score[1] == min([score[1] for score in evaluations])]
+
+            return choice(moves)
--- Exercise3/connect_4_minimax.py
+++ Exercise3/connect_4_minimax.py
@@ -3,7 +3,7 @@ from two_player_games.player import Player
 from two_player_games.games.connect_four import ConnectFour, ConnectFourMove
 import time
 from random import choice
-ROW_COUNT = 6
+ROW_COUNT = 5
 COLUMN_COUNT = 7
 
 if __name__ == "__main__":
@@ -12,9 +12,8 @@ if __name__ == "__main__":
 
     # Dobre głębokości przeszukiwań
     # 2, 4, 6, 8, 10
-    DEPTH = 4
+    DEPTH = 2
 
-    i = 0
     p1_won = 0
     p2_won = 0
     games = 1
@@ -22,36 +21,26 @@ if __name__ == "__main__":
         game = ConnectFour(size=(COLUMN_COUNT, ROW_COUNT), first_player=p1,
                            second_player=p2)
 
-        algorithm = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT)
+        algorithm = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT, max_player=p1, min_player=p2)
 
-        algorithm2 = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT)
+        algorithm2 = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT, max_player=p2, min_player=p1)
 
-        heuristic_sovler = HeuristicSolver(game, ROW_COUNT, COLUMN_COUNT)
+        heuristic_sovler = HeuristicSolver(game, ROW_COUNT, COLUMN_COUNT, max_player=p1, min_player=p2)
 
+        i = 0
         while not algorithm.game.state.is_finished():
             time.sleep(0.1)
 
-            """chosen_column = input("Column: ")
-            game.make_move(ConnectFourMove(int(chosen_column)))"""
-
-
-
             i += 1
             # MiniMax alpha-beta
-            best_move = algorithm.get_best_move(DEPTH)
-            #best_move = heuristic_sovler.get_heuristic_move()
+            #best_move = algorithm.get_best_move(DEPTH)
+            #best_move = heuristic_sovler.get_best_move()
+            best_move = int(input("Column: "))
 
             # Random move
             #best_move = choice(algorithm.get_valid_moves())
 
             # Heuristic move
-            """scores = []
-            for move in algorithm.get_valid_moves():
-                board_copy = algorithm.simulate_move(algorithm.game.state, move)
-                scores.append((move, algorithm.evaluate_position(board_copy, p1)))
-
-
-            best_move = max(scores, key=lambda x: x[1])[0]"""
             game.make_move(ConnectFourMove(best_move))
             print(game)
 
@@ -82,4 +71,7 @@ if __name__ == "__main__":
     print(f"P1 winnings stats\t Won: {p1_won}\tLoosed: {p2_won} \tWin percent: {round((p1_won/games)*100, 2)}")
     print(
         f"P1 winnings stats\t Won: {p2_won}\tLoosed: {p1_won} \tWin percent: {round((p2_won / games) * 100, 2)}")
+
+
+
 
