699ed18463cfeacf2c953adb716aeee7fd7a5696
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2023-11-18 12:33:07 +0100

    Latest changes

--- Exercise3/connect_4_minimax.py
+++ Exercise3/connect_4_minimax.py
@@ -1,3 +1,4 @@
+import time
 from typing import Tuple, List
 
 from two_player_games.player import Player
@@ -21,23 +22,22 @@ class MinMaxSolver:
 
         self._prizes = {
             "center": 4.,
-            "plr_two_in_seg": 2.,
-            "plr_three_in_seg": 10.,
-            "plr_winning_cond": 1000.,
-            "opp_two_in_seg": 2,
-            "opp_winning_cond": 100
+            "plr_two_in_seg": 1e2,
+            "plr_three_in_seg": 1e4,
+            "plr_winning_cond": 1e10,
+            "opp_two_in_seg": 1,
+            "opp_winning_cond": 1e6
         }
+
 
     def evaluate_position(self, board: ConnectFourState,
                           player: Player) -> float:
+
         prize = 0
         # Center bonus
         center_column = board.fields[self._column_count // 2]
         center_count = center_column.count(player)
-        if self.game.first_player:
-            prize += self._prizes["center"] * center_count
-        else:
-            prize -= self._prizes["center"] * center_count
+        prize += self._prizes["center"] * center_count
 
         # Check verticals
         for i in range(self._column_count):
@@ -57,11 +57,27 @@ class MinMaxSolver:
                 segment = [board.fields[i + z][j + z] for z in range(4)]
                 prize += self._evaluate_segment(segment, player)
 
+                # Ostateczność
+                """if player == self.game.first_player:
+                    if segment.count(player) == 2 and segment.count(None) == 2:
+                        prize += self._prizes["plr_two_in_seg"] / 2
+                else:
+                    if segment.count(player) == 2 and segment.count(None) == 2:
+                        prize -= self._prizes["plr_two_in_seg"] / 2"""
+
         # Check falling edge diagonals
         for i in range(self._column_count - 3):
             for j in range(3, self._row_count):
                 segment = [board.fields[i + z][j - z] for z in range(4)]
                 prize += self._evaluate_segment(segment, player)
+
+                # Ostateczność
+                """if player == self.game.first_player:
+                    if segment.count(player) == 2 and segment.count(None) == 2:
+                        prize += self._prizes["plr_two_in_seg"] / 2
+                else:
+                    if segment.count(player) == 2 and segment.count(None) == 2:
+                        prize -= self._prizes["plr_two_in_seg"] / 2"""
 
         return prize
 
@@ -72,28 +88,16 @@ class MinMaxSolver:
 
         prize = 0
 
-        if player == self.game.first_player:
-            if segment.count(player) == 4:
-                prize += self._prizes["plr_winning_cond"]
-            elif segment.count(player) == 3 and segment.count(None) == 1:
-                prize += self._prizes["plr_three_in_seg"]
-            elif segment.count(player) == 2 and segment.count(None) == 2:
-                prize += self._prizes["plr_two_in_seg"]
-            elif segment.count(opponent) == 3 and segment.count(None) == 1:
-                prize -= self._prizes["opp_winning_cond"]
-            elif segment.count(opponent) == 2 and segment.count(None) == 2:
-                prize -= self._prizes["opp_two_in_seg"]
-        else:
-            if segment.count(player) == 4:
-                prize -= self._prizes["plr_winning_cond"]
-            elif segment.count(player) == 3 and segment.count(None) == 1:
-                prize -= self._prizes["plr_three_in_seg"]
-            elif segment.count(player) == 2 and segment.count(None) == 2:
-                prize -= self._prizes["plr_two_in_seg"]
-            elif segment.count(opponent) == 3 and segment.count(None) == 1:
-                prize += self._prizes["opp_winning_cond"]
-            elif segment.count(opponent) == 2 and segment.count(None) == 2:
-                prize += self._prizes["opp_two_in_seg"]
+        if segment.count(player) == 4:
+            prize += self._prizes["plr_winning_cond"]
+        elif segment.count(player) == 3 and segment.count(None) == 1:
+            prize += self._prizes["plr_three_in_seg"]
+        elif segment.count(player) == 2 and segment.count(None) == 2:
+            prize += self._prizes["plr_two_in_seg"]
+        elif segment.count(opponent) == 3 and segment.count(None) == 1:
+            prize -= self._prizes["opp_winning_cond"]
+        elif segment.count(opponent) == 2 and segment.count(None) == 2:
+            prize -= self._prizes["opp_two_in_seg"]
 
         return prize
 
@@ -113,166 +117,87 @@ class MinMaxSolver:
         return [valid_column for valid_column in range(COLUMN_COUNT) if
                 self.is_valid_move(valid_column)]
 
-    def get_best_move(self) -> int:
+    def get_best_move_2(self, depth: int) -> int:
+        col, score = self.minimax_prune(self.game.state, depth, -np.inf, np.inf, True)
+        return col
+
+    def get_best_move_3(self, depth: int):
         valid_moves = self.get_valid_moves()
-
-        player = self.game.state.get_current_player()
-
-        if player == self.game.first_player:
-            best_prize = -np.inf
-        else:
-            best_prize = np.inf
-        best_move = choice(valid_moves)
+        results = []
 
         for move in valid_moves:
             board_copy = copy(self.game.state)
             board_copy = board_copy.make_move(ConnectFourMove(move))
-            print(board_copy)
-            prize = self.evaluate_position(board_copy, player)
-            if player == self.game.first_player:
-                if prize > best_prize:
-                    best_prize = prize
-                    best_move = move
-            else:
-                if prize < best_prize:
-                    best_prize = prize
-                    best_move = move
+            results.append(self.minimax_prune(board_copy, depth, -np.inf, np.inf, True))
 
-        print(f"Best move: {best_move}")
-        return best_move
+        print(results)
+        max_results = [result[0] for result in results if result[1] == max([result[1] for result in results])]
+        return choice(max_results)
 
-    def is_terminal(self) -> bool:
-        """
-        Check if the board state is a terminal state meaning one of the players
-        is currently winning
-        """
-
-        # Check verticals
-        for i in range(self._column_count):
-            for j in range(self._row_count - 3):
-                segment = [self.game.state.fields[i][j + z] for z in range(4)]
-                if segment.count(self.game.first_player) == 4 or segment.count(
-                        self.game.second_player) == 4:
-                    return True
-
-        # Check horizontals
-        for i in range(self._column_count - 3):
-            for j in range(self._row_count):
-                segment = [self.game.state.fields[i + z][j] for z in range(4)]
-                if segment.count(self.game.first_player) == 4 or segment.count(
-                        self.game.second_player) == 4:
-                    return True
-
-        # Check rising edge diagonals
-        for i in range(self._column_count - 3):
-            for j in range(self._row_count - 3):
-                segment = [self.game.state.fields[i + z][j + z] for z in
-                           range(4)]
-                if segment.count(self.game.first_player) == 4 or segment.count(
-                        self.game.second_player) == 4:
-                    return True
-
-        # Check falling edge diagonals
-        for i in range(self._column_count - 3):
-            for j in range(3, self._row_count):
-                segment = [self.game.state.fields[i + z][j - z] for z in
-                           range(4)]
-                if segment.count(self.game.first_player) == 4 or segment.count(
-                        self.game.second_player) == 4:
-                    return True
-
-        # Check if the board is not completely full
-        if sum([column.count(None) for column in game.state.fields]) == 0:
-            return True
-
-        return False
 
     def minimax_prune(self, board, depth, alpha: float, beta: float,
                 is_maximizing_player: bool) -> Tuple[int, float]:
         """Returns column index and score"""
         valid_moves = self.get_valid_moves()
-        is_terminal = self.is_terminal()
-        beta = (choice(valid_moves), beta)
-        alpha = (choice(valid_moves), alpha)
+        is_terminal = board.is_finished()
 
         if is_terminal or depth == 0:
-            if is_maximizing_player:
-                return (self.get_best_move(), self.evaluate_position(board, self.game.first_player))
+            if is_terminal:
+                if board.get_winner() == self.game.first_player:
+                    return (None, 1e9)
+                elif board.get_winner() == self.game.second_player:
+                    return (None, -1e9)
+                else:
+                    return (None, 0)
             else:
-                values = (self.get_best_move(),
-                 self.evaluate_position(board, self.game.second_player))
-                #print(f"MININIZING: \t{values}")
-                return values
+                return (None, self.evaluate_position(board, self.game.second_player))
 
         if is_maximizing_player:
+            value = -np.inf
+            chosen_column = choice(valid_moves)
             for valid_move in valid_moves:
                 board_copy = copy(board)
                 board_copy = board_copy.make_move(ConnectFourMove(valid_move))
 
-                new_alpha = self.minimax_prune(board_copy, depth - 1, alpha[1], beta[1], False)
+                evaluation = self.minimax_prune(board_copy, depth - 1, alpha, beta, False)[1]
+                if evaluation > value:
+                    value = evaluation
+                    chosen_column = valid_move
 
-                alpha = max(alpha, new_alpha, key=lambda x: x[1])
+                alpha = max(alpha, value)
 
-                if alpha[1] >= beta[1]:
+                if alpha >= beta:
                     break
-            print(f"Chosen alpha: {beta}\tDepth: {depth}")
-            return alpha
+
+            return chosen_column, value
 
         else:
+            value = np.inf
+            chosen_column = choice(valid_moves)
             for valid_move in valid_moves:
                 board_copy = copy(board)
                 board_copy = board_copy.make_move(ConnectFourMove(valid_move))
 
-                new_beta = self.minimax_prune(board_copy, depth - 1, alpha[1], beta[1], True)
+                evaluation = self.minimax_prune(board_copy, depth - 1, alpha, beta, True)[1]
 
-                print(f"Available: {beta[1]}\t{new_beta[1]}")
-                beta = min(beta, new_beta, key=lambda x: x[1])
+                if evaluation < value:
+                    value = evaluation
+                    chosen_column = valid_move
+                beta = min(beta, value)
 
-                if alpha[1] >= beta[1]:
+                if alpha >= beta:
                     break
 
-            print(f"Chosen beta: {beta}\tDepth: {depth}")
-            return beta
+            return chosen_column, value
 
-    def minimax(self, board, depth, is_maximizing_player: bool) -> Tuple[int, float]:
-        """Returns column index and score"""
+    def run_evaluations(self, player):
         valid_moves = self.get_valid_moves()
-        is_terminal = self.is_terminal()
 
-        if is_terminal or depth == 0:
-            if is_maximizing_player:
-                return (self.get_best_move(), self.evaluate_position(board, self.game.first_player))
-            else:
-                values = (self.get_best_move(),
-                 self.evaluate_position(board, self.game.second_player))
-                return values
-
-        if is_maximizing_player:
-            max_value = (choice(valid_moves), -np.inf)
-            for valid_move in valid_moves:
-                board_copy = copy(board)
-                board_copy = board_copy.make_move(ConnectFourMove(valid_move))
-
-                evaluation = self.minimax(board_copy, depth - 1, False)
-
-                max_value = max(max_value, evaluation, key=lambda x: x[1])
-
-            return max_value
-
-        else:
-            min_value = (choice(valid_moves), np.inf)
-            for valid_move in valid_moves:
-                board_copy = copy(board)
-                board_copy = board_copy.make_move(ConnectFourMove(valid_move))
-
-                evaluation = self.minimax(board_copy, depth - 1, True)
-
-                min_value = min(min_value, evaluation, key=lambda x: x[1])
-
-            return min_value
-
-
-
+        for move in valid_moves:
+            board_copy = copy(self.game.state)
+            board_copy = board_copy.make_move(ConnectFourMove(move))
+            prize = self.evaluate_position(board_copy, player)
+            print(f"Move: {move}\tPrize: {prize}")
 
 
 """if __name__ == "__main__":
@@ -302,33 +227,41 @@ class MinMaxSolver:
 """
 
 if __name__ == "__main__":
-    p1 = Player("a")
-    p2 = Player("b")
-    DEPTH = 5
+    p1 = Player("x")
+    p2 = Player("o")
+    DEPTH = 3
     game = ConnectFour(size=(COLUMN_COUNT, ROW_COUNT), first_player=p1,
                        second_player=p2)
 
     algorithm = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT)
+    player = p1
 
-    while not algorithm.is_terminal():
-        input("Please type enter...")
+    i = 0
+    #print(game)
+    while not algorithm.game.state.is_finished():
+        time.sleep(0.1)
+        i += 1
 
-        print(f"Playing AI: {p1.char}")
-        best_move_p1, score_p1 = algorithm.minimax(algorithm.game.state, DEPTH,
-                                                   True)
-        print(f"Maximizing: {best_move_p1}\t{score_p1}")
-        game.make_move(ConnectFourMove(best_move_p1))
+        """chosen_column = input("Column: ")
+        game.make_move(ConnectFourMove(int(chosen_column)))
 
-        print(f"Playing AI: {p2.char}")
-        best_move_p2, score_p2 = algorithm.minimax(algorithm.game.state, DEPTH,
-                                                   False)
-        game.make_move(ConnectFourMove(best_move_p2))
-        print(f"Minimizing: {best_move_p2}\t{score_p2}")
+        if algorithm.game.state.is_finished():
+            break"""
+
+        #print(f"Playing AI: {player.char}")
+        best_move = algorithm.get_best_move_3(DEPTH)
+        game.make_move(ConnectFourMove(best_move))
 
         print(game)
 
+
+
+
+
     print(f"Won: {game.get_winner().char}")
     print("Terminated")
+    print(f"Moves: {i}")
+
 
 """if __name__ == "__main__":
     p1 = Player("a")
@@ -340,9 +273,7 @@ if __name__ == "__main__":
     algorithm = MinMaxSolver(game, ROW_COUNT, COLUMN_COUNT)
 
     game.make_move(ConnectFourMove(3))
-    game.make_move(ConnectFourMove(0))
-    game.make_move(ConnectFourMove(3))
-    game.make_move(ConnectFourMove(1))
     print(game)
-    print(algorithm.get_best_move())
+    print(algorithm.run_evaluations(p2))
+    print(algorithm.get_best_move_2())
     print(game.state.get_current_player().char)"""
