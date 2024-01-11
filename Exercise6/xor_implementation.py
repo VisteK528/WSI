1856e2add80076c9cb12e68f431f494602c8e390
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2024-01-11 20:45:21 +0100

    Fixed XOR implementation from Exercise6

--- Exercise6/xor_implementation.py
+++ Exercise6/xor_implementation.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from utils import (one_hot_encode, cross_entropy_loss, cross_entropy_loss_derivative,
                    squared_error, squared_error_derivative)
+from optimizers import SimpleSGD
 
 
 if __name__ == "__main__":
@@ -17,22 +18,21 @@ if __name__ == "__main__":
     print(y[:10])
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
-    y_train = one_hot_encode(y_train)
+    #y_train = one_hot_encode(y_train)
+    y_train = np.array([np.array([y]) for y in y_train])
 
     # Small test model
     layers = [FullyConnected(input_size=2, output_size=2), Tanh(),
-              FullyConnected(input_size=2, output_size=1), Tanh(),
-              Linear()]
+              FullyConnected(input_size=2, output_size=1), Tanh()]
 
-    #loss = Loss(cross_entropy_loss, cross_entropy_loss_derivative)
     loss = Loss(squared_error, squared_error_derivative)
+    optimizer = SimpleSGD(learning_rate=0.1, decay=0.1)
 
-    net = Network(layers, learning_rate=0.1)
-    net.compile(loss)
+    net = Network(layers)
+    net.compile(loss, optimizer)
+    net.fit(x_train, y_train, epochs=100, batch_size=50, verbose=1)
 
-    #net.load_parameters("data/iris.npy")
-    #net.fit(x_train, y_train, 1, verbose=1, batch_size=10, learning_rate=0.3)
-    #net.save_parameters("data/iris.npy")
-
-    accuracy = net.evaluate(x_test, y_test)
-    print(f"Accuracy: {accuracy*100:.2f}%")
+    y_test = [np.array(y) for y in y_test]
+    for x, y in zip(x_test[:10], y_test[:10]):
+        pred = net(x)
+        print(y, pred)
